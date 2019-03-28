5c392627b38c703d79d74b8dbc1bbcfcab78792f
frbravom <>
2019-03-27 22:40:14 -0300

    ejercicio1

---
+++ contenido/respListar.php
@@ -0,0 +1,83 @@
+<?php
+include('../lib/vacaciones.php');
+include('../lib/constantes.php');
+
+if(isset($_SESSION["aVacaciones"])){
+    $arrVacaciones=$_SESSION["aVacaciones"];    
+}
+ else {
+     $arrVacaciones="";
+}
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?=$_SESSION["hm"];?></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+        <link href="../css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <div id="contenedor">
+            <div id="titulo"></div>
+            <div id="menu"><?php include('../menu.php');?></div>
+                    <div id="contenido">
+                                               
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>Rut</th>
+                                <th>Nombre</th>
+                                <th>Cargo</th>
+                                <th>Fecha Inicio</th>
+                                <th>Fecha Fin</th>
+                                <th>Días</th>
+                                <th>Comentario</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                foreach ($arrVacaciones as $key => $oVacacion) {
+                                ?>
+                                <tr>
+                                <td>#<?=$key +1;?></td>
+                                <td><?=$oVacacion->getRut();?></td>
+                                <td><?=$oVacacion->getNombre();?></td>
+                                <td>Cargo</td>
+                                <td>Fecha Inicio</td>
+                                <td>Fecha Fin</td>
+                                <td>Días</td>
+                                <td>Comentario</td>
+                                <td><input type="button" value="Eliminar" onclick="javascript:feliminar(<?=$oVacacion->getRut();?>);"></td>
+                              </tr>
+                               <?php }?>
+                            </tbody>
+                </table>
+                    </div>
+
+        </div>
+        <div id="resultado"></div>
+        <form id="formelimina" action="../lib/eliminar.php" method="post">
+            <input type="hidden" name="rut" id="rut">
+        </form>
+    </body>
+    
+    <script>
+        $("[id*=subsolicitud-infvtas]").show();
+        $("#subsolicitud-infvtas").addClass("active");
+        $("[id*=subsolicitud]").css("display","block");
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
+</html>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="list-group">
+<a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Ventas Express</a>
+<a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Ingresos"><i class="fa fa-file-o fa-2x"></i> Ingresos</a>
+<a id="subsolicitud-ingprod" class="list-group-item submenu" href="<?=URLBASE;?>contenido/ingresoproductos.php" ><i class="fa fa-sun-o fa-2x"></i> Productos</a>
+<a id="subsolicitud-ingvtas" class="list-group-item submenu" href="<?=URLBASE;?>contenido/ingresoventas.php" ><i class="fa fa-sun-o fa-2x"></i>Ventas</a>
+<a id="subsolicitud-ingbod" class="list-group-item submenu" href="<?=URLBASE;?>contenido/ingresobodega.php" ><i class="fa fa-sun-o fa-2x"></i>Bodega</a>
+<a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Informes"><i class="fa fa-file-o fa-2x"></i> Informes</a>
+<a id="subsolicitud-infvtas" class="list-group-item submenu" href="<?=URLBASE;?>contenido/informeventas.php" ><i class="fa fa-sun-o fa-2x"></i>Ventas</a>
+<a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Consultas"><i class="fa fa-file-o fa-2x"></i>Consultas</a>
+<a id="subsolicitud-conprod" class="list-group-item submenu" href="<?=URLBASE;?>contenido/consultaproductos.php" ><i class="fa fa-sun-o fa-2x"></i>Productos</a>
+</div>
+      
